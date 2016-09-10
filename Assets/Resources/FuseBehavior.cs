b518acbc9e8b32441e191f64fd7fa51b10c87f42
MuAdd <Mu.Add@Zoho.com>
2016-09-09 18:03:02 -0700

    Add blinking light and text to Fuse.prefab

--- Assets/Resources/FuseBehavior.cs
+++ Assets/Resources/FuseBehavior.cs
@@ -3,15 +3,28 @@ using System.Collections;
 
 public class FuseBehavior : MonoBehaviour {
 
+    Color lerpedColor;
+    float lerpedFloat = 0;
+    float lightIntensity;
+    float maxIntensity = 8;
+    public Material lightEmittingDiode;
     GameObject[] bin = new GameObject[8];
     Vector3 liftup = Vector3.up * 0.2f; 
     // Use this for initialization
     void Start () {
-            bin = GameObject.FindGameObjectsWithTag("FuseBin");
+        bin = GameObject.FindGameObjectsWithTag("FuseBin");
     }
 	
+    void blinkEffect() {
+        lerpedFloat = Mathf.PingPong(Time.time, 1);
+        lerpedColor = Color.Lerp(Color.black, Color.green, lerpedFloat);
+        lightEmittingDiode.color = lerpedColor;
+        gameObject.GetComponentInChildren<Light>().intensity = lerpedFloat * maxIntensity;
+    }
+
 	// Update is called once per frame
 	void Update () {
+        
         foreach (GameObject fusebin in bin)
         {
             Bounds binBounds = fusebin.transform.GetComponent<MeshCollider>().bounds;
@@ -22,4 +35,8 @@ public class FuseBehavior : MonoBehaviour {
                 }
         }
 	}
+
+    void FixedUpdate() {
+        blinkEffect();
+    }
 }
